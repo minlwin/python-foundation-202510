85ce5defdbee07a229c273c8906e5b7f63ade848
minlwin <lwin.zawmin@gmail.com>
2026-03-09 16:21:10 +0630

    Update Exceptions

--- Chapter07/application/src/main.py
+++ Chapter07/application/src/main.py
@@ -1,32 +1,50 @@
-from src.lib.accounts import AccountInput, AccountManager
+from src.lib.accounts import AccountInput, AccountManager, AccountError
 from src.lib.operations import OperationBase, BalanceCheckOperation, DepositOperation, WithdrawOperation
 
 class Application:
     def __init__(self, accounts : list[AccountInput]) -> None:
         self._account_manager = AccountManager(accounts)
+        self._operations = None
 
     def launch(self):
         _show_message("Welcome to ATM")
         
-        self._login_operation()
+        retry = 0
 
-        while True:
-            operation = self._get_operation()
-            if operation == None:
-                break;
-            operation.execute()
+        while retry < 3 and self._operations == None:
+            try:
+                self._login_operation()
+                while True:
+                    operation = self._get_operation()
+                    if operation == None:
+                        break;
+                    try:
+                        operation.execute()
+                    except AccountError as error:
+                        print(f"Error : {error.message}\n")
+            except AccountError as error:
+                print(f"Error : {error.message}\n")
+                retry += 1
+                if retry < 3:
+                    print("Please login again.")
+            except Exception:
+                print("System Error")
+                break
+            except BaseException:
+                print("Force Quite Request")
+                break
 
         _show_message("Thank You")
 
     
-    def _get_operation(self)->OperationBase:
+    def _get_operation(self):
         print("--------------------------")
         print("Operations")
         for operation in self._operations.values():
             operation.show_menu()
         print()
 
-        menu_id = input("Enter ID : ")
+        menu_id = input("Select Operation : ")
         return self._operations.get(menu_id, None)
 
 
